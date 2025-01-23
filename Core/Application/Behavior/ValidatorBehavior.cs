1244c6a9ba6d7ae8d93a69cacf377c1e1ffd98a5
johan <johan.adc@hotmail.com>
2025-01-23 23:20:03 +0100

    Add validation behavior

---
+++ Core/Application/Behavior/ValidatorBehavior.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using FluentValidation;
+
+namespace Application.Behavior;
+
+public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest: IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidatorBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+    
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            var failures = validationResults.SelectMany(v => v.Errors);
+            
+            if (failures.Any())
+                throw new ValidationException(failures);
+        }
+        
+        return await next();
+    }
+}
